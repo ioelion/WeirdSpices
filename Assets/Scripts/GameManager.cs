816de138ca7d2d87c98956298d455fd17a2bead9
ioelion <51096656+ioelion@users.noreply.github.com>
2023-07-23 18:42:52 -0300

    GameManager y BoardManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class GameManager : MonoBehaviour
+{
+        public static GameManager instance = null;
+
+        public BoardManager boardScript;
+
+        private int level = 3;
+  
+    void Awake() {
+        if(instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+        DontDestroyOnLoad(gameObject);
+        boardScript = GetComponent<BoardManager>();
+        InitGame();
+    }
+
+    void InitGame()
+    {
+        boardScript.SetupScene(level);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
