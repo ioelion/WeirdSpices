99d6d95c8ad21453b15f4ad0c9c5c5d13dcccb7f
ioelion <51096656+ioelion@users.noreply.github.com>
2023-07-24 12:38:27 -0300

    Ajuste colliders + mecanica de agarrar y soltar semilla

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -28,6 +28,7 @@ public class Player : MonoBehaviour
         {
             //movementSpeed.animator.SetBool("walking", true);
             rb.velocity = _force;
+            //TODO yoelpedemonte cambiar sprite personaje por uno de costado
             sr.flipX = Mathf.Sign(_force.x) < 0;
         }
         else
@@ -35,5 +36,26 @@ public class Player : MonoBehaviour
             //manager.animator.SetBool("walking", false);
             rb.velocity = Vector2.zero;
         }
+
+        if(Input.GetKey(KeyCode.Space)){
+            //TODO yoelpedemonte objeto a donde se adhieran para evitar problemas con el detach children 
+            Transform tfchildren = this.transform.GetChild(0);
+            tfchildren.position = new Vector2(this.transform.position.x + 2, this.transform.position.y + 2);
+            this.transform.DetachChildren();
+        }
+        
     }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag.Equals("Ingredient"))
+        {
+            //TODO yoelpedemonte agregar limite de hijos (semillas)
+            other.gameObject.transform.parent = this.transform;
+            other.transform.position = new Vector2(this.transform.position.x, this.transform.position.y + 1);
+
+        }
+    }
+
+
 }
