dd4a88d93272afad1e60b39e3177fe53579d1843
ioelion <51096656+ioelion@users.noreply.github.com>
2023-07-24 11:23:59 -0300

    PlayerMovement Rudimentario + limpieza de scripts innecesarios

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,92 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
-public class Player : MovingObject
+public class Player : MonoBehaviour
 {
-    public int wallDamage = 1;
-    public int pointsPerFood = 10;
-    public int pointsPerSoda = 20;
-    public float restartLevelDelay = 1f;
+    [SerializeField]
+    private int movementSpeed;
 
-    private Animator animator;
-    private int food;
+    private Rigidbody2D rb;
 
+    private SpriteRenderer sr;
     // Start is called before the first frame update
-    protected override void Start()
+    void Start()
     {
-        animator = GetComponent<Animator>();
-        food = GameManager.instance.playerFoodPoints;
-        base.Start();
-    }
-
-    private void OnDisable() {
-        GameManager.instance.playerFoodPoints = food;
+        rb = this.GetComponent<Rigidbody2D>();
+        sr = this.GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(!GameManager.instance.playersTurn) return;
-        int horizontal = 0;
-        int vertical = 0;
+        float _x = Input.GetAxis("Horizontal") * movementSpeed;
+        float _y = Input.GetAxis("Vertical") * movementSpeed;
+        Vector2 _force = new Vector2(_x, _y);
 
-        horizontal = (int) Input.GetAxisRaw("Horizontal");
-        vertical = (int) Input.GetAxisRaw("Vertical");
-
-        if(horizontal != 0)
-            vertical = 0;
-        
-        if(horizontal != 0 || vertical != 0)
-            AttemptMove<Wall> (horizontal, vertical);
-
-    }
-
-    protected override void AttemptMove <T> (int xDir, int yDir)
-    {
-        food--;
-        base.AttemptMove <T> (xDir, yDir);
-        RaycastHit2D hit;
-        CheckIfGameOver();
-        GameManager.instance.playersTurn = false;
-    }
-
-    private void OnTriggerEnter2D(Collider2D other) 
-    {
-        if(other.tag == "Exit"){
-            Invoke("Restart", restartLevelDelay);
-            enabled = false;
-        } else if (other.tag == "Food"){
-            food += pointsPerFood;
-            other.gameObject.SetActive(false);
-        } else if (other.tag == "Soda"){
-            food += pointsPerSoda;
-            other.gameObject.SetActive(false);
-        } 
-    }
-
-    protected override void OnCantMove <T> (T component)
-    {
-        Wall hitWall = component as Wall;
-        hitWall.DamageWall(wallDamage);
-        animator.SetTrigger("playerChop");
-    }
-
-    private void CheckIfGameOver()
-    {
-        if(food<=0)
-            GameManager.instance.GameOver();
-    }
-    
-    private void Restart()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex,LoadSceneMode.Single);
-    }
-
-    public void LoseFood(int loss){
-        animator.SetTrigger("playerHit");
-        food -= loss;
-        CheckIfGameOver();
+        if (_force != Vector2.zero)
+        {
+            //movementSpeed.animator.SetBool("walking", true);
+            rb.velocity = _force;
+            sr.flipX = Mathf.Sign(_force.x) < 0;
+        }
+        else
+        {
+            //manager.animator.SetBool("walking", false);
+            rb.velocity = Vector2.zero;
+        }
     }
 }
