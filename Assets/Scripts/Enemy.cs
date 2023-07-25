cf1e7e70d379d4adc48d5c029db3a8f6e2f7c1c0
ioelion <51096656+ioelion@users.noreply.github.com>
2023-07-25 19:26:51 -0300

    Persecución al jugador y animaciones enemigas

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,19 +5,57 @@ using UnityEngine;
 namespace WeirdSpices{
     public class Enemy : MonoBehaviour
     {
+        private Transform target;    
+        private Rigidbody2D rb;
+
         [SerializeField]
-        private Transform target;
+        private float moveSpeed;
+
+        private Vector2 moveDirection;
+        private Vector2 _force;
+        private SpriteRenderer sr;
+        private Animator an;
 
         // Start is called before the first frame update
         void Start()
         {
-            
+            rb = this.GetComponent<Rigidbody2D>();
+            sr = this.GetComponent<SpriteRenderer>();
+            an = this.GetComponent<Animator>();
+        }
+
+        void Awake() {
+            target = GameObject.Find("Player").transform;
+        }
+
+        void Update() {
+            if(target){
+                Vector3 direction = (target.position - transform.position).normalized;
+                float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+                rb.rotation = angle;
+                moveDirection = direction;
+            }    
         }
 
         // Update is called once per frame
         void FixedUpdate()
         {
-            transform.position = Vector2.MoveTowards(transform.position, target.position, Time.deltaTime * 10f);
+            if(target){
+                _force = new Vector2(moveDirection.x, moveDirection.y) * moveSpeed;
+                if (_force != Vector2.zero)
+                {   
+                    an.SetBool("playerWalk", true);
+                    rb.velocity = _force;
+                    rb.SetRotation(0);
+                    sr.flipX = Mathf.Sign(_force.x) > 0;
+                }
+                else
+                {
+                    an.SetBool("playerWalk", false);
+                    rb.velocity = Vector2.zero;
+                }
+            }
+
         }
     }
 
