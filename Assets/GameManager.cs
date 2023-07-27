625aeb587be39f20c41ac551ef20806ad7ae1750
ioelion <51096656+ioelion@users.noreply.github.com>
2023-07-26 23:06:38 -0300

    Reiniciar el juego tras pantalla de fin

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using TMPro;
 
 public class GameManager : MonoBehaviour
@@ -18,7 +19,10 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.R)){
+            ResumeGame();
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex,LoadSceneMode.Single);
+        }
     }
 
     public void EndGame(){
