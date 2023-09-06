60c8d801d74b921c5f1eb2b188bad8bf32ce4487
luca <ochoa.luca@gmail.com>
2023-09-06 12:09:34 -0300

    Sonidos Ataque, heal y moneda

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -6,11 +6,13 @@ using WeirdSpices;
 public class Coin : Dropable
 {
     [SerializeField] private int value = 1;
+    [SerializeField] private AudioClip sound;
 
     private void OnTriggerEnter2D(Collider2D col)
     {
         if(col.CompareTag("Player"))
         {
+            AudioManager.Instance.PlaySound(sound);                     //new
             Destroy(this.gameObject);
             GameManager.Instance.GainGold(value);
             GameManager.Instance.RemoveToList(this.gameObject);
